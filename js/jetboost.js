27c127824431913a8222d7b509fc305f9a627d0f
babunewgit1 <babunewx11@gmail.com>
2024-12-16 21:25:03 +0600

    before revision 1 16-12-2024

--- js/jetboost.js
+++
@@ -1,5 +0,0 @@
-{
-  /* <script id="jetboost-script" type="text/javascript"> window.JETBOOST_SITE_ID = "cm3lh8pr3003b0yu06ydt4l2y"; (function(d) { var s = d.createElement("script"); s.src = "https://cdn.jetboost.io/jetboost.js"; s.async = 1; d.getElementsByTagName("head")[0].appendChild(s); })(document); </script> */
-}
-
-// https://markuphero.com/share/XWlRlaGhVJjfoN0di3rm
