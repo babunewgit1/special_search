067a56370e38c33c4abd5f924d7a1f58e48b7025
babunewgit1 <babunewx11@gmail.com>
2024-12-31 11:06:36 +0600

    api data passing format

---
+++ postman.js
@@ -0,0 +1,56 @@
+// one way
+
+/*
+from airport id:1498584208298x684827003860846200
+to airport id:1498585359850x624785352498292900
+date_as_text:2025-01-25
+time_as_text:11:45 AM
+App_Out_Date_As_Text:2025-01-25
+pax:2
+date:1737741600
+*/
+
+// round way
+/*
+out-dep airport id:1498593287145x262154499068856240
+out-arr airport id:1498592891086x376001300057396300
+out-dep date:2024-12-18
+out-pax:2
+ret-date:2024-12-28
+ret-dep airport id:1498592891086x376001300057396300
+ret-arr airport id:1498593287145x262154499068856240
+ret-pax:2
+Dep_date_as_text:2024-12-28
+Ret_date_as_text:2024-12-29
+Dep_time_as_text:1734458400
+Ret_time_as_text:1734458400
+App_Out_Date_As_Text:2024-12-28
+App_Ret_Date_As_Text:2024-12-28
+*/
+
+// multicity {json}
+/*
+{
+  "from airport id": ["1498587098475x839985227445140500","1498590229233x449956897646188740"],
+  "to airport id": ["1498587086693x162732264492660770", "1498587131344x159802006557583800"],
+  "date": ["1735149600","1735581600"],
+  "pax": ["2","2"],
+  "date_as_text": ["2024-12-26","2024-12-31"],
+  "time_as_text": ["12:00 AM","12:00 AM"],
+  "App_Out_Date_As_Text": ["2024-12-26","2024-12-31"]
+}
+
+*/
+
+//broker_request
+
+/*
+aircraftids:["1498587098475x839985227445140500","1498590229233x449956897646188740"]
+message:sdfsdf
+
+*/
+
+//send_message
+/*
+
+*/
